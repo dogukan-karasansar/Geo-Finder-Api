4c3df50d7658d3e31edde0c102d913d816f98ce2
Doğukan Karasansar <myfa_96@hotmail.com>
2024-04-11 14:46:35 +0300

    Add RateLimitProvider to handle API rate limiting

---
+++ app/Providers/RateLimitProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\ServiceProvider;
+
+class RateLimitProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        RateLimiter::for('api', function ($request) {
+            return Limit::perMinute(30)
+                ->by(optional($request->user())->id ?: $request->ip());
+        });
+    }
+}
