2ed3d34fe30589d16f6fb4e2d79b473529cb08e4
Doğukan Karasansar <myfa_96@hotmail.com>
2024-04-21 03:55:44 +0300

    namespace fixed

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,10 @@ Route::post('/v1/token', '\App\Api\Auth\Controllers\AuthController@token');
 Route::group([
     'prefix' => 'v1',
     'middleware' => 'auth:sanctum',
+    'namespace' => 'App\Api'
 ], function () {
-    Route::apiResource('countries', 'App\Api\Countries\Controllers\CountryController')
+    Route::apiResource('countries', 'Countries\Controllers\CountryController')
         ->only('index');
-    Route::apiResource('cities', 'App\Api\Cities\Controllers\CityController')
+    Route::apiResource('cities', 'Cities\Controllers\CityController')
         ->only('index');
 });
